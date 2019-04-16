b2edb4b9b2c5b19884764067dc238808a2acd58b
Andrew Bastin <andrewbastin.k@gmail.com>
2019-04-16 10:13:42 +0530

    Updated status bar, fills the entire row, pos info rendered on right edge

--- src/status/status.c
+++ src/status/status.c
@@ -6,15 +6,22 @@ void status_run(struct Editor *editor) {
 
     curses_activateAttr(CURSES_ATTR_REVERSE);
 
+    // Printing spaces to fill the full bottom row
+    for (int i = 0; i < curses_getScreenWidth(); i++) {
+        curses_drawChar(curses_getScreenHeight() - 1, i, ' ');
+    }
+
     curses_printf(
         curses_getScreenHeight() - 1, 
         0, 
-        "%s [%d lines]\t %d, %d", 
+        " %s [%d lines]", 
         (editor->filename == NULL) ? "<New File>" : editor->filename, 
-        editor->lineCount, 
-        editor->cursY + 1, 
-        editor->cursX + 1
+        editor->lineCount
     );
+
+    // Rendering position string on the right edge
+    size_t sizeNeeded = snprintf(NULL, 0, "%d, %d", editor->cursY + 1, editor->cursX + 1) + 1; // Calculating the size of the string
+    curses_printf(curses_getScreenHeight() - 1, curses_getScreenWidth() - sizeNeeded, "%d, %d", editor->cursY + 1, editor->cursX + 1);
     
     curses_deactivateAttr(CURSES_ATTR_REVERSE);
 
