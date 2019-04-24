ed616b7a4f47b14235019a81c87c5324a1f9ce4f
Andrew Bastin <andrewbastin.k@gmail.com>
2019-04-24 12:28:10 +0530

    Added draw with color and present functions to renderer

--- renderer.c
+++ renderer.c
@@ -35,9 +35,21 @@ void renderer_drawText(int y, int x, char *str, int len) {
    }
 }
 
+/* Draw text with color */
+void renderer_drawTextWithColor(int y, int x, char *str, int len, uint16_t fg, uint16_t bg) {
+    for (int pos = 0; (pos < len) && (x + pos < renderer_getScreenWidth()); pos++) {
+        renderer_drawCharWithColor(y, x + pos, str[pos], fg, bg); 
+    }
+}
+
 /* Clears the curses screen */
 void renderer_clear() {
     tb_clear();
+}
+
+/* Draws the backbuffer on screen */
+void renderer_present() {
+    tb_present();
 }
 
 /* Sets the rendered cursor position */
