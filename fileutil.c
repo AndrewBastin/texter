8a977caef53f25aec5cc867486b9621e5dbc66b2
Andrew Bastin <andrewbastin.k@gmail.com>
2019-05-02 21:14:21 +0530

    Added fileutil function to check whether file is writable

--- fileutil.c
+++ fileutil.c
@@ -1,5 +1,13 @@
+#include <sys/stat.h>
+
 #include "unistd.h"
 
+short isFileWritable(char *filename) {
+    struct stat s;
+    short code = stat(filename, &s);
+
+    return (code == 0) && (s.st_mode & W_OK);
+}
 
 short file_exists(char *filename) {
     return access(filename, F_OK) != -1;
--- fileutil.h
+++ fileutil.h
@@ -1,2 +1,5 @@
+/* Returns 1 if whether the file exists and the file is writable */
+short isFileWritable(char *filename);
+
 /* Returns 1 if file exists else returns 0 */
 short file_exists(char *filename);
